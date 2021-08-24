12f7b6cb47bc0cbf8d6b885ba91913cec221dbf1
Sushant Pokharel <53488923+Gr8Sushant@users.noreply.github.com>
2021-08-24 22:48:17 +0545

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const App = () => {
   return(
     <ChatEngine
       height = "100vh"
-      projectID = "d54115bd-bfda-44b5-ad90-8b8c1959b1fa"
-      userName = "johnydoye"
-      userSecret = "test123"
+      projectID = ""
+      userName = ""
+      userSecret = ""
     />
   )
 }
